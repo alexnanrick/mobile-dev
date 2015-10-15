80a7c321b63bfa006d544f0537a442974b715256
Alex Kiernan <zontzor@gmail.com>
2015-10-15 10:49:05 +0100

    Added lab 5 bits

---
+++ Lab5-MoreLists/app/src/main/java/com/zontzor/lab5_morelists/China.java
@@ -0,0 +1,20 @@
+package com.zontzor.lab5_morelists;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by nanrick on 15/10/15.
+ */
+public class China extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.country_layout);
+
+        String country;
+
+
+    }
+}
--- Lab5-MoreLists/app/src/main/java/com/zontzor/lab5_morelists/MainActivity.java
+++ Lab5-MoreLists/app/src/main/java/com/zontzor/lab5_morelists/MainActivity.java
@@ -1,11 +1,15 @@
 package com.zontzor.lab5_morelists;
 
 import android.app.ListActivity;
+import android.content.Intent;
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends ListActivity {
 
@@ -26,10 +30,30 @@ public class MainActivity extends ListActivity {
         countries[8] = "Germany";
         countries[9] = "USA";
 
-        setListAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, countries));
+        setListAdapter(new ArrayAdapter<>(this, R.layout.mylist,R.id.Itemname,countries));
+
+        //setListAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, countries));
     }
 
-    public void onListItemClick(ListView list, View v, int position, long id) {
+    public void onListItemClick(ListView listView, View v, int position, long id) {
 
+        switch( position )
+        {
+            case 0:  Intent newActivity1 = new Intent(this, Ireland.class);
+                startActivity(newActivity1);
+                break;
+            case 1:  Intent newActivity2 = new Intent(this, SriLanka.class);
+                startActivity(newActivity2);
+                break;
+            case 2:  Intent newActivity3 = new Intent(this, SaudiArabia.class);
+                startActivity(newActivity3);
+                break;
+            case 3:  Intent newActivity4 = new Intent(this, China.class);
+                startActivity(newActivity4);
+                break;
+            case 4:  Intent newActivity5 = new Intent(this, Colombia.class);
+                startActivity(newActivity5);
+                break;
+        }
     }
 }
