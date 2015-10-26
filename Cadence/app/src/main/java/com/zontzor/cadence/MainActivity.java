d5cb36cabcef967888d6bf100b3a8a8f6c4a3eba
Zontzor <alex.kiernan@mydit.ie>
2015-10-26 12:02:20 +0000

    Added custom adapter and image assests

--- Cadence/app/src/main/java/com/zontzor/cadence/MainActivity.java
+++ Cadence/app/src/main/java/com/zontzor/cadence/MainActivity.java
@@ -1,13 +1,33 @@
 package com.zontzor.cadence;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+    GridView gridView;
+    static final String[] options = new String[] {
+            "Profile", "Rides","Goals", "Cadence" };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setupGrid();
+    }
+
+    public void setupGrid() {
+        gridView = (GridView) findViewById(R.id.menu_grid);
+        gridView.setAdapter(new MyAdapter(this, options));
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+
+            }
+        });
     }
 }
---
+++ Cadence/app/src/main/java/com/zontzor/cadence/MyAdapter.java
@@ -0,0 +1,80 @@
+package com.zontzor.cadence;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by Zontzor on 2015-10-26.
+ */
+public class MyAdapter extends BaseAdapter {
+    private Context context;
+    private final String[] menuOptions;
+
+    public MyAdapter(Context context, String[] menuOptions) {
+        this.context = context;
+        this.menuOptions = menuOptions;
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        LayoutInflater inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View gridView;
+
+        if (convertView == null) {
+
+            gridView = new View(context);
+
+            // get layout from menu_gridview.xml
+            gridView = inflater.inflate(R.layout.menu_gridview, null);
+
+            // set value into textview
+            TextView textView = (TextView) gridView
+                    .findViewById(R.id.grid_item_label);
+            textView.setText(menuOptions[position]);
+
+            // set image based on selected text
+            ImageView imageView = (ImageView) gridView
+                    .findViewById(R.id.grid_item_image);
+
+            String option = menuOptions[position];
+
+            if (option.equals("Profile")) {
+                imageView.setImageResource(R.mipmap.menu_user_profile);
+            } else if (option.equals("Rides")) {
+                imageView.setImageResource(R.mipmap.menu_rides);
+            } else if (option.equals("Goals")) {
+                imageView.setImageResource(R.mipmap.menu_goals);
+            } else if (option.equals("Cadence")){
+                imageView.setImageResource(R.mipmap.menu_cadence);
+            }
+
+        } else {
+            gridView = (View) convertView;
+        }
+
+        return gridView;
+    }
+
+    @Override
+    public int getCount() {
+        return menuOptions.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+}
